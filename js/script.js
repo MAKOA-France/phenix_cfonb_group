8cac411a2767ba3d839628b162eacec27f12347f
Usha Matisson <matisson@makoa.fr>
2024-08-23 12:25:28 +0200

    asset injector

--- js/script.js
+++ js/script.js
@@ -4,6 +4,114 @@
       attach: function(context, settings) {
 
         $(document).ready(function() {
+
+
+          /*********** asset injector */
+
+
+          /*************Enlever "index.php" dans les lien de documents   GLOBAL ********************/
+          // Get the current href attribute
+          var currentHref = jQuery('.path-media .field--type-file.field--name-field-media-document.field--widget-file-generic span a').attr('href');
+
+          // Remove "index.php" from the href attribute
+          var modifiedHref = currentHref.replace('index.php/', '');
+
+          // Update the href attribute with the modified value
+          jQuery('.path-media .field--type-file.field--name-field-media-document.field--widget-file-generic span a').attr('href', modifiedHref);
+
+
+          /*****************configureHourEvent     /civicrm-event/* ***********/
+          let pageEvent = window.location.href.includes('civicrm-event');
+          if (pageEvent) {
+            document.addEventListener("DOMContentLoaded", function(event) { 
+
+              // Listener user get date
+              document.getElementById("edit-start-date-0-value-date").addEventListener('input', function(){
+                 //get time put by user
+                 var dateToBegin = document.getElementById('edit-start-date-0-value-date').value;
+                  document.getElementById('edit-end-date-0-value-date').value = dateToBegin;
+              
+              }, true);
+              
+              
+              
+              
+              // Listener user get time
+              document.getElementById("edit-start-date-0-value-time").addEventListener('input', function(){
+              
+               //add duration format => '00:00'
+               var duration  = '01:30';
+              
+              //transform time add to Array
+              var durationArray = duration.split(':');
+              
+              
+              
+              //get time put by user
+              var timeToBegin = document.getElementById('edit-start-date-0-value-time').value;
+              
+              //transform to Array
+              var timeToBeginArray = timeToBegin.split(':'); 
+              
+              var timeToBeginFinal;
+                      
+              //get hour
+              var hour = +timeToBeginArray[0];
+              
+               //get minute
+              var minutes = +timeToBeginArray[1];
+              
+              //add hour begining to hour duration
+              hour+=+durationArray[0]
+              
+              //add minute begining to minute duration
+              minutes+=+durationArray[1];
+              
+              console.log(hour);
+              
+              if (+minutes > 59)
+              {
+                  hour+=1;
+                  console.log(+minutes);
+                   minutes = minutes - 60;
+                   console.log(+minutes);
+              
+                   if (minutes === 0) {
+                      minutes = "00";
+                      minutes.toString();
+              
+                      // timeToBeginFinal = hour + ':'+minutes;
+                   }
+                   else
+                   if (minutes < 9) {
+                      minutes = '0'+minutes;
+                      minutes.toString();
+                      // timeToBeginFinal = hour + ':'+minutes;
+                   }
+              
+              }
+              if (hour < 10) {
+                  hour = '0'+hour;
+                  hour.toString();
+              }else if (hour > 23) {
+                  hour = hour - 24;
+                  hour = '0'+hour;
+                  hour.toString();
+              }
+              timeToBeginFinal = hour + ':'+minutes;
+              document.getElementById('edit-end-date-0-value-time').value = timeToBeginFinal.toString();
+              
+              }, true);
+              
+              });
+          }
+
+
+          /**************End asset injector  */
+
+
+
+
           $('#printerParticipants').on('click', function () {
             printContent() 
           });
